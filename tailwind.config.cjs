e29e027f94649e8b45b25b475663049be549c8b3
Yann Rabiller <yann.rabiller@fortrabbit.com>
2022-11-13 13:47:57 +0100

    Add feed page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				'rss-light-mode': "url('https://res.cloudinary.com/dybam16gk/image/upload/v1668335093/blog_images/rss_zp3rq2.png')",
+				'rss-dark-mode': "url('https://res.cloudinary.com/dybam16gk/image/upload/v1668335093/blog_images/rss-white_eyvvye.png')",
+			}
+		},
 	},
 	plugins: [],
 }
