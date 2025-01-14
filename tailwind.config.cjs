89bf3c0d6b34712ffcbca4eae3d3bee5478d0b22
Einenlum <contact@einenlum.com>
2025-01-15 00:21:11 +0100

    The beauty of goofy diagrams

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			backgroundImage: {
-				'rss-light-mode': "url('https://res.cloudinary.com/dybam16gk/image/upload/v1668335093/blog_images/rss_zp3rq2.png')",
-				'rss-dark-mode': "url('https://res.cloudinary.com/dybam16gk/image/upload/v1668335093/blog_images/rss-white_eyvvye.png')",
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'rss-light-mode':
+          "url('https://res.cloudinary.com/dybam16gk/image/upload/v1668335093/blog_images/rss_zp3rq2.png')",
+        'rss-dark-mode':
+          "url('https://res.cloudinary.com/dybam16gk/image/upload/v1668335093/blog_images/rss-white_eyvvye.png')",
+      },
+      fontFamily: {
+        comic: ['Comic Sans MS', 'Comic Sans', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+};
