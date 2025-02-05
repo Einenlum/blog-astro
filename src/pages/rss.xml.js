667291a64c52585c254a5e36aa355174538c4fd1
Einenlum <contact@einenlum.com>
2025-02-06 00:03:40 +0100

    Fix RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,26 +1,30 @@
 import rss from '@astrojs/rss';
 
-const articleImportResult = import.meta.glob('./articles/*.mdx', { eager: true });
+const articleImportResult = import.meta.glob('./articles/*.mdx', {
+  eager: true,
+});
 const articles = Object.values(articleImportResult);
 const recentArticles = articles.sort((a, b) => b.publishedAt - a.publishedAt);
 
-export const get = () => rss({
-  // `<title>` field in output xml
-  title: 'Einenlum',
-  // `<description>` field in output xml
-  description: "Einenlum's blog",
-  // base URL for RSS <item> links
-  // SITE will use "site" from your project's astro.config.
-  site: import.meta.env.SITE,
-  // list of `<item>`s in output xml
-  // simple example: generate items for every md file in /src/pages
-  // see "Generating items" section for required frontmatter and advanced use cases
-  items: recentArticles.map((article) => ({
-    title: article.title,
-    description: article.description,
-    link: article.url,
-    pubDate: article.publishedAt,
-})),
-  // (optional) inject custom xml
-  customData: `<language>en-us</language>`,
-});
+export function GET(context) {
+  return rss({
+    // `<title>` field in output xml
+    title: 'Einenlum',
+    // `<description>` field in output xml
+    description: "Einenlum's blog",
+    // base URL for RSS <item> links
+    // SITE will use "site" from your project's astro.config.
+    site: context.site,
+    // list of `<item>`s in output xml
+    // simple example: generate items for every md file in /src/pages
+    // see "Generating items" section for required frontmatter and advanced use cases
+    items: recentArticles.map((article) => ({
+      title: article.title,
+      description: article.description,
+      link: article.url,
+      pubDate: article.publishedAt,
+    })),
+    // (optional) inject custom xml
+    customData: `<language>en-us</language>`,
+  });
+}
